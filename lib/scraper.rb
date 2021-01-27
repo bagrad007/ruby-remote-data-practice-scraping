1edd6dee7bce12f78e1952b2eb05b68788ae9ac4
Luke Gill <bagrad007@gmail.com>
2021-01-27 17:10:56 -0500

    commit tree 4449a537c0090d6f9ca349286deed0e4a58c25f4

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,14 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
+doc = Nokogiri::HTML(html)
+
 html = open("https://flatironschool.com/")
+
+doc.css(".site-header__hero__headline")
+courses = doc.css(".tout__label.heading.heading--level-4")[0]
+binding.pry
+courses.each do |course|
+    puts course.text.strip
+end
